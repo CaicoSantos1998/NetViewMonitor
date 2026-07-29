3cfc7bdd15e5a28accb359c00d16f87844b75f0c
CS <caico_fc@hotmail.com>
2026-07-29 14:23:01 -0300

    Chore: Add .gitignore file with common exclusions.

---
+++ src/java/main/com/github/caicosantos1998/ntvm/repository/CsvRepository.java
@@ -0,0 +1,42 @@
+package java.main.com.github.caicosantos1998.ntvm.repository;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.main.com.github.caicosantos1998.ntvm.model.PingResult;
+import java.main.com.github.caicosantos1998.ntvm.service.MetricsTracker;
+import java.time.format.DateTimeFormatter;
+
+public class CsvRepository {
+    private final String filePath;
+    private final DateTimeFormatter dateTimeFormatter;
+
+    public CsvRepository(String filePath, DateTimeFormatter dateTimeFormatter) {
+        this.filePath = filePath;
+        this.dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        initializeHeader();
+    }
+
+    private void initializeHeader() {
+        File file = new File(filePath);
+        if (!file.exists() || file.length() == 0) {
+            try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
+                writer.println("Hour,Status,Ping_ms,min_Ping,max_Ping,average_Ping");
+            } catch (IOException e) {
+                System.err.println("Failed to Initialize the CSV file :" + e.getMessage());
+            }
+        }
+    }
+
+    public void log(PingResult result, MetricsTracker tracker) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
+            String timeStampFormate = result.timeStamp().format(dateTimeFormatter);
+            String row = String.format("%s,%s,%d,%d,%d,%d", timeStampFormate, result.status().name(),
+                    result.latencyMs(), tracker.getMinPing(), tracker.getMaxPing(), tracker.getAveragePing());
+            writer.println(row);
+        } catch (IOException e) {
+            System.err.println("Error writing data to the file: " + e.getMessage());
+        }
+    }
+}
